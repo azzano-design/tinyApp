c42f4207363e04f01b99884caebed452b02e4f2b
Brendan <brendanarchangelo@gmail.com>
2017-12-05 21:22:17 +0000

    Fixed partial path

--- express_server.js
+++ express_server.js
@@ -3,8 +3,9 @@ const app = express();
 const PORT = process.env.PORT || 8080; // default port 8080
 const bodyParser = require("body-parser");
 
-app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -23,16 +24,16 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
-// GET FOR URLS
-app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
-});
-
 // POST FOR URLS
 app.post("/urls", (req, res) => {
   console.log(req.body);  // debug statement to see POST parameters
   res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+// GET FOR URLS
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
