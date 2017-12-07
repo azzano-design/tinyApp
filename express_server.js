b7f06f302880c1c1fe44b2c6a5c3fef0acd77a1b
Brendan <brendanarchangelo@gmail.com>
2017-12-07 20:18:27 +0000

    Added redirect function to the long URL when the short URL is called at /u/shortURL

--- express_server.js
+++ express_server.js
@@ -24,40 +24,53 @@ function generateRandomString() {
   return(shortendURL);
 }
 
-// GET REQUESTS
+// HOME PAGE
 app.get("/", (req, res) => {
   res.end("Hello!");
 });
 
+// JSON LIST
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+// NEW URLS PAGE
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+// URL LISTING
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+// URL LISTING ADD
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // debug statement to see POST parameters
+  let shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
+  console.log(urlDatabase);
+  res.redirect(`/urls/${shortURL}`);
+});
+
+// SHORT URL SHOW
 app.get("/urls/:id", (req, res) => {
-  let templateVars = { shortURL: req.param.id };
+  let templateVars = { shortURL: req.params.id };
   res.render("urls_show", templateVars);
+});
+
+// LONG URL REDIRECT
+app.get("/u/:shortURL", (req, res) => {
+  let shortURL = req.params.shortURL;
+  let longURL = urlDatabase[shortURL];
+  let templateVars = { longURL: longURL }
+  res.redirect(longURL);
 });
 
 app.get("/hello", (req, res) => {
   res.end("<html><body>Hello <b>World!</b></body></html>\n");
 });
-
-
-// POST REQUESTS
-app.post("/urls", (req, res) => {
-  console.log(req.body);  // debug statement to see POST parameters
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
-});
-
 
 // PORT LISTENING AT
 app.listen(PORT, () => {
