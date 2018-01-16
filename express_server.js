d57b5eba57d68d2b00fad017247dab4c1f039ea3
Brendan <brendanarchangelo@gmail.com>
2018-01-16 13:34:25 +0000

    final refactor, included bcrypt from missed commit and minor styles

--- express_server.js
+++ express_server.js
@@ -2,32 +2,20 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8080; // default port 8080
 const bodyParser = require("body-parser");
-const cookieParser = require('cookie-parser')
+const cookieSession = require('cookie-session');
+const bcrypt = require('bcrypt');
+const methodOverride = require('method-override');
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(cookieParser());
+app.use(cookieSession({
+  name: 'session',
+  keys: ["BruceWayneisBatman"]
+}));
+app.use(methodOverride('_method'));
 
 
-var users = {
-  "userRandomID": {
-    id: "userRandomID",
-    email: "bruce@waynetech.com",
-    password: "deadparents123"
-  },
- "user2RandomID": {
-    id: "user2RandomID",
-    email: "jackwhite@acelabs.com",
-    password: "H4H4H4!"
-  }
-}
-
-var urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-};
-
-
+// FUNCTIONS
 function generateRandomString() {
   let potentialChar = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
   let string_length = 6;
@@ -39,128 +27,263 @@ function generateRandomString() {
   return(shortendURL);
 }
 
-// REGISTER PAGE
-app.get("/register", (req, res) => {
-  let templateVars = { user_id: req.cookies["user_id"] };
-  res.render("register", templateVars);
+function ifExists(itemToCheck, database, property) {
+  for (let id in database) {
+    if (itemToCheck === database[id][property]){
+      console.log('Item exists');
+      return true
+      // item exists already
+    }
+  }
+  console.log('Item does not exist');
+  return false;
+}
+
+function checkPassword(email, password, users){
+  for(let user in users){
+    //if email exists
+    if(email === users[user].email){
+      //compare encrypted password
+      if(bcrypt.compareSync(password, users[user].password)){
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+function getUserByID(email, usersDatabase){
+  for(let user in usersDatabase){
+    if(email === usersDatabase[user].email){
+      return usersDatabase[user].id;
+    }
+  }
+  return undefined;
+}
+
+function getURLSByUser(userID){
+  let urlList = {};
+  for(let url in urlDatabase){
+    if(urlDatabase[url].ownerID === userID){
+      urlList[url] = urlDatabase[url];
+    }
+  }
+  return urlList;
+}
+
+// DATABASES
+// # USERS
+
+const usersDatabase = {
+  "b4tm4N": {
+    id: "b4tm4N",
+    email: "bruce@waynetech.com",
+    password: bcrypt.hashSync("dead-parents", 11)
+  },
+ "J0K3Rr": {
+    id: "J0K3Rr",
+    email: "jackwhite@acelabs.com",
+    password: bcrypt.hashSync("egg-yolks", 11)
+  }
+}
+// # LINKS
+const urlDatabase = {
+  "b2xVn2": {
+    id: "b2xVn2",
+    longURL: "http://www.lighthouselabs.ca",
+    ownerID: "b4tm4N"
+  },
+  "9sm5xK": {
+    id: "9sm5xK",
+    longURL: "http://www.dccomics.com",
+    ownerID: "J0K3Rr"
+  }
+};
+
+app.use(function(req, res, next){
+  res.locals.user = usersDatabase[req.session["user_id"]];
+  req.loggedIn = !!res.locals.user;
+  next();
 });
 
-// REGISTER ADD
-app.post("/register", (req, res) => {
-  let email = req.body.email;
-  let password = req.body.password;
-  let userDatabase = Object.keys(users);
-  var userExists = false;
+app.use("/urls", function(req, res, next){
+  if(req.loggedIn){
+    next();
+  }else{
+    res.redirect('/login');
+  }
+});
 
+app.use((req, res, next) => {
+  if(!req.session.visitorId){
+    req.session.visitorId = generateRandomString();
+  }
+  next();
+})
+
+// REGISTER PAGE
+app.get('/register', (req, res) => {
+  if(req.loggedIn){
+    res.status(301).redirect('/urls');
+    return;
+  } else{
+    res.render('register');
+  }
+})
+
+// REGISTER ADD
+app.post('/register', function(req, res){
   // Check if form is actually entered
-  if (password === '' || email === '') {
+  if(!req.body.email || !req.body.password){
     res.status(400);
-    res.send('You must enter both an email and password');
+    res.send("You must enter both an email and password");
     return;
   }
   // Check database if entered email already exists
-  for(var i = 0; i < userDatabase.length; i++) {
-    existingEmail = users[userDatabase[i]].email;
-    if (existingEmail == email) {
-      userExists = true;
-    }
-  }
   // If it does, send nasty error
-  // If it doesn't add them to userDatabase
-  if ( userExists === true ){
+  if(ifExists(req.body.email, usersDatabase, "email")){
     res.status(400);
-    res.send('User already exists, please use a different email');
-  } else {
-      id = generateRandomString();
-      user = { id: id, email: email, password: password};
-      users[id] = user;
-      res.cookie('user_id', id);
-      res.redirect('/urls');
+    res.send("User already exists, please use a different email");
+    return;
   }
-});
-
-
-// LOGIN
-app.post("/login", (req, res) => {
-  let user_id = req.body.username;
-  res.cookie('user_id', user_id);
-  let templateVars = { user_id: req.cookies["user_id"] };
+  // If it doesn't add them to usersDatabase
+  const id = generateRandomString();
+  usersDatabase[id] = {
+    "id" : id,
+    "email" : req.body.email,
+    "password" : bcrypt.hashSync(req.body.password, 11)
+  };
+  req.session["user_id"] = usersDatabase[id].id;
+  console.log(usersDatabase);
   res.redirect('/urls');
 });
+
+
+// LOGIN PAGE
+app.get("/login", (req, res) => {
+  if(req.loggedIn){
+    res.status(301).redirect('/urls');
+    return;
+  }
+  res.render('login');
+});
+
+
+// LOGIN SUBMIT
+app.post('/login', function(req, res){
+  if(ifExists(req.body.email, usersDatabase, "email")){
+    if(checkPassword(req.body.email, req.body.password, usersDatabase)){
+      const userId = getUserByID(req.body.email, usersDatabase);
+      req.session["user_id"] = userId;
+      res.redirect('/urls');
+    }else{
+      res.status(403);
+      res.send("Invalid information. Please try again");
+      console.log('Invalid password');
+    }
+  }else{
+    res.status(403);
+    res.send("Invalid information. Please try again");
+    console.log('Invalid username');
+  }
+})
 
 // LOGOUT
-app.post("/logout", (req, res) => {
-  res.clearCookie('user_id');
+app.post('/logout', function(req, res){
+  req.session = null;
   res.redirect('/urls');
-});
+})
 
 // HOME PAGE
 app.get("/", (req, res) => {
   res.redirect('/urls');
 });
 
-// JSON LIST
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
 // NEW URLS PAGE
-app.get("/urls/new", (req, res) => {
-  let templateVars = { user_id: req.cookies["user_id"] };
-  res.render("urls_new", templateVars);
+app.get('/urls/new', (req, res) => {
+  res.render("urls_new");
 });
 
 // URL LISTING
-app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase, user_id: req.cookies["user_id"] };
-  res.render("urls_index", templateVars);
+app.get('/urls', function(req, res){
+  res.render('urls_index', {
+    urls: getURLSByUser(req.session["user_id"])
+  });
+  console.log(urlDatabase);
 });
 
 // URL LISTING ADD
-app.post("/urls", (req, res) => {
-  let shortURL = generateRandomString();
-  urlDatabase[shortURL] = req.body.longURL;
-  let templateVars = { user_id: req.cookies["user_id"] };
+app.post('/urls', (req, res) => {
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = {
+    id: shortURL,
+    longURL: req.body.longURL,
+    ownerID: usersDatabase[req.session["user_id"]].id
+  };
+  console.log(urlDatabase);
   res.redirect(`/urls/${shortURL}`);
 });
 
 // SHORT URL SHOW
-app.get("/urls/:id", (req, res) => {
-  let templateVars = { shortURL: req.params.id, urlDatabase: urlDatabase, user_id: req.cookies["user_id"] };
-  res.render("urls_show", templateVars);
+app.get('/urls/:id', function(req, res){
+  const templateVars = {
+    url : urlDatabase[req.params.id],
+    shortURL : req.params.id,
+    longURL: urlDatabase[req.params.id].longURL
+  };
+  console.log(urlDatabase);
+  res.status(200).render('urls_show', templateVars);
+  return;
 });
 
+app.use('/urls/:id', function(req, res, next){
+  if(ifExists(req.params.id, urlDatabase, "id")){
+    next();
+  }else{
+    res.status(400).send("Invalid request");
+  }
+})
+
 // SHORT URL UPDATE
-app.post("/urls/:id", (req, res) => {
-  let shortURL = req.params.id;
-  let newURL = req.body.newURL;
-  urlDatabase[shortURL] = newURL;
-  let templateVars = { user_id: req.cookies["user_id"] };
-  res.redirect('/urls');
+app.post('/urls/:id', function(req, res){
+  // If URL belongs to owner, allow edit
+  if(res.locals.user.id === urlDatabase[req.params.id].ownerID){
+    urlDatabase[req.params.id].longURL = req.body.longURL;
+    res.status(200);
+    res.redirect('/urls');
+  }else{
+    res.status(401);
+    res.send("Sorry! Only the owner can edit this link");
+  }
 })
 
 // SHORT URL DELETE
-app.post("/urls/:id/delete", (req, res) => {
-  let shortURL = req.params.id;
-  let shortURLString = shortURL.toString();
-  let templateVars = { urls: urlDatabase, user_id: req.cookies["user_id"] };
-  delete templateVars.urls[shortURLString];
-  res.redirect('/urls');
+app.delete('/urls/:id/delete', function(req, res){
+  // If URL belongs to owner, allow delete
+  if(res.locals.user.id === urlDatabase[req.params.id].ownerID){
+    delete urlDatabase[req.params.id];
+    res.status(200);
+    res.redirect("/urls");
+  }else{
+    res.status(401);
+    res.send("Sorry! Only the owner can delete this link");
+  }
 });
 
 // LONG URL REDIRECT
 app.get("/u/:shortURL", (req, res) => {
-  let shortURL = req.params.shortURL;
-  let longURL = urlDatabase[shortURL];
-  let templateVars = { longURL: longURL, username: req.cookies["username"] }
-  res.redirect(longURL);
-});
-
-app.get("/hello", (req, res) => {
-  res.end("<html><body>Hello <b>World!</b></body></html>\n");
+  const shortURL = req.params.shortURL;
+  if(shortURL in urlDatabase){
+    const longURL = urlDatabase[shortURL].longURL;
+    if(longURL){
+      res.redirect(longURL);
+      return;
+    }
+  }
+  res.status(404).send("Page Not Found");
 });
 
 // PORT LISTENING AT
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+  console.log(`Shrinky-Links listening on port ${PORT}!`);
 });
